80708c4a1058c2fd4bfa57bd0287e0a7ceba5d65
Mixmp29 <denisov.mihail2013@yandex.ru>
2024-03-19 19:46:11 +0700

    Write constructor for class Server

--- src/TCP/Server.cpp
+++ src/TCP/Server.cpp
@@ -1,1 +1,24 @@
 #include "TCP.hpp"
+
+Server::Server() {
+  if ((sockMain = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("Сервер не может открыть главный socket");
+    exit(1);
+  }
+
+  bzero((char *)&servAddr, sizeof(servAddr));
+  servAddr.sin_family = AF_INET;
+  servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servAddr.sin_port = 0;
+
+  if (bind(sockMain, (struct sockaddr *)&servAddr, sizeof(servAddr))) {
+    perror("Связывание сервера неудачно.");
+    exit(1);
+  }
+
+  printf("SERVER: Номер sid: %d\n", sockMain);
+  printf("SERVER: IP-адресс: %d\n", ntohs(servAddr.sin_addr.s_addr));
+  printf("SERVER: номер порта - % d\n\n", ntohs(servAddr.sin_port));
+
+  listen(sockMain, NTHRDS);
+}
