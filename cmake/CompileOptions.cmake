cd24711392c4467127cff7e2adff037c9e08d809
Mixmp29 <denisov.mihail2013@yandex.ru>
2024-03-20 16:24:52 +0700

    Update c++ standart to 20

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -2,13 +2,13 @@ function(set_compile_options target_name)
   if(MSVS)
     target_compile_options(${target_name} PRIVATE /W4 /WX)
   else()
-    target_compile_options(${target_name} PRIVATE -Wall -pthread)
+    target_compile_options(${target_name} PRIVATE -std=c++20 -Wall -lpthread)
   endif()
 
   set_target_properties(
     ${target_name}
     PROPERTIES
-      CXX_STANDARD 17
+      CXX_STANDARD 20
       CXX_STANDARD_REQUIRED ON
       CXX_EXTENSIONS OFF
   )
