a75fb655ed3c54d86fce89024fdf5e180daf6799
Mixmp29 <denisov.mihail2013@yandex.ru>
2024-03-20 19:47:51 +0700

    Initialize method run from class Client Initialize method threadsend from class Client Initialize method threadrecv from class Client Initialize method send_msg from class Client Initialize method recv_msg from class Client

--- src/TCP/Client.cpp
+++ src/TCP/Client.cpp
@@ -1,4 +1,5 @@
 #include <TCP/TCP.hpp>
+#include <iostream>
 
 Client::Client(char *ip, int port) {
   struct hostent *hp;
@@ -23,9 +24,74 @@ void Client::run() {
     printf("Успешное подключение к серверу.\n");
   }
 
-  std::thread thrd1(Client::send_msg, this, sockMain);
-  std::thread thrd2(Client::send_msg, this, sockMain);
+  std::thread thrd1(&Client::threadsend, this, sockMain);
+  std::thread thrd2(&Client::threadrecv, this, sockMain);
 
   thrd1.join();
   thrd2.join();
+}
+
+int Client::threadsend(int sockClient) {
+  int Client = sockClient;
+
+  char name[BUFLEN];
+  char message[BUFLEN];
+  char buf[BUFLEN];
+  printf("What is your name?\n\n");
+  scanf("%s", name);
+  printf("\nYou can type message now %s\n\n", name);
+  strcat(name, ".");
+  for (;;) {
+    bzero(buf, BUFLEN);
+    std::cin.getline(message, BUFLEN);
+    strcat(buf, name);
+    strcat(buf, message);
+    if (strlen(message) > 0) {
+      // send
+      send_msg(buf, Client);
+      //
+    }
+  }
+
+  close(Client);
+  return 0;
+}
+
+int Client::threadrecv(int sockClient) {
+  int Client = sockClient;
+
+  char buf[BUFLEN];
+  int msgLength = 0;
+  for (int i = 1;; ++i) {
+    // recv
+    recv_msg(buf, msgLength, Client);
+    //
+
+    if (msgLength == 0) {
+      printf("\nBreak in recv\n");
+      break;
+    }
+
+    printf("\n%s\n\n", buf);
+  }
+
+  close(Client);
+  return 0;
+}
+
+void Client::send_msg(char *buf, int sockClient) {
+  if (send(sockClient, buf, BUFLEN, MSG_NOSIGNAL) < 0) {
+    perror("Проблемы с пересылкой.\n");
+    exit(1);
+  }
+}
+
+void Client::recv_msg(char *buf, int &msgLength, int sockClient) {
+  bzero(buf, BUFLEN);
+  if ((msgLength = recv(sockClient, buf, BUFLEN, 0)) < 0) {
+    perror("Плохое получение потоком\n");
+    printf("sockClient in pthread: %d\n", sockClient);
+    printf("msgLength in pthread: %d\n", sockClient);
+    exit(1);
+  }
 }
