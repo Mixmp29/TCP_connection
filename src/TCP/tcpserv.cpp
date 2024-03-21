1e17411e6f3d4e4c76882aa1792bbd976f790c60
Mixmp29 <denisov.mihail2013@yandex.ru>
2024-03-21 07:18:39 +0700

    Add processing of command line arguments

--- src/TCP/tcpserv.cpp
+++ src/TCP/tcpserv.cpp
@@ -1,8 +1,13 @@
 #include <TCP/TCP.hpp>
 
-int main() {
-  Server server;
-  server.in_work();
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    Server server(atoi(argv[1]));
+    server.in_work();
+  } else {
+    Server server;
+    server.in_work();
+  }
 
   return 0;
 }
